3f5a664991c827f86f338e3dbb54fc94a12197b4
Tharshini98 <70389909+Tharshini98@users.noreply.github.com>
2025-12-23 12:08:37 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -13,5 +13,10 @@ app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/profile', profileRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Backend is running ");
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
